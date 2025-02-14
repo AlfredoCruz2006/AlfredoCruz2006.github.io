cca274c4a735319c774068cd3d778b7f80c4aa76
AlfredoCruz2006 <alfredo.cruz@jpschools.org>
2025-02-14 21:42:51 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -31,20 +31,25 @@ $(function () {
 
 
     // TODO 2 - Create Platforms
-
-
-
+    createPlatform(600, 40, 200, 260);
+    createPlatform(1200, 350, 70, 40, "red");
+    createPlatform(1150, 450, 70, 40, "red");
+    createPlatform(1200, 550, 70, 40, "red");
+    createPlatform(1150, 650, 70, 40, "red");
+    createPlatform(1150, 250, 70, 40, "red");
 
     // TODO 3 - Create Collectables
-
-
-
+    createCollectable("steve", 1200, 150);
+    createCollectable("diamond", 200, 170, 0.5, 0.7);
+    createCollectable("grace", 400, 170, 0.5, 0.7);
     
     // TODO 4 - Create Cannons
-
-
-    
-    
+    createCannon("top", 200, 400);
+    createCannon("right", 300, 2000);
+    createCannon("top", 400, 400);
+    createCannon("top", 600, 400);
+    createCannon("top", 800, 400);
+    createCannon("top", 1000, 400);
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
